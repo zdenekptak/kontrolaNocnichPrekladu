9d53d8e5e8b916b7d83e727fe35c98fa12c39f2a
zdenekptak <zptak@seznam.cz>
2020-10-05 16:14:18 +0200

    Přidání prijemcu do confug kterým se email pošle jen v případě chyby. V run.py taktéž přidání odesílání mailu v případě ok nebo ko výsledku.  Do preklad.py byla dodělána část pro případ že není soubor nalezen. Také se podle toho mění předmět emailu a nastavuje se závažnost emailu. V případě nesplnění je email s vysokou důležitostí.

--- preklad.py
+++ preklad.py
@@ -9,59 +9,123 @@ from email import encoders
 import json
 
 class Preklady:
+    def __init__(self):
+        pass
     
-    def kontrola(configCesty):
-
-        with open(configCesty) as json_file:
-
-            configCesty = json.load(json_file)            
-
-
+    def kontrolaVerze(self, verzeHeliosu, souborCesta):
+        self.verzeHeliosu = verzeHeliosu
+        self.souborCesta = souborCesta
         vysledkyPrekladuSouboru = []
-        for verzeHeliosu, souborCesta in configCesty.items():
-            for soubor, cesta in souborCesta.items():
-                nowDate = datetime.now() # current date and time
-                datumDnesniDen = int(nowDate.strftime("%d"))
+        vysledkyOKKO = []
+        for soubor, cesta in self.souborCesta.items():
+            nowDate = datetime.now() # current date and time
 
+            datumDnesniDen = int(nowDate.strftime("%d"))
+
+            try:
                 datumSouboru = time.ctime(os.path.getmtime(cesta))
-    #             print(f"Poslední změna {s} proběhla: {datumSouboru}")
-
-                datumVytvoreniSouboru = time.ctime(os.path.getctime(cesta))
-    #             print(f"Souboru vutvoren: \n {datumVytvoreniSouboru}")
-
                 datumSouboruDen = [int(s) for s in datumSouboru.split() if s.isdigit()][0]
-                if datumSouboruDen == datumDnesniDen:
-                    vyslZprava = (f"{verzeHeliosu} {soubor} je OK")
-                    vysledkyPrekladuSouboru.append([vyslZprava])
-                else:
-                    vyslZprava = (f"{verzeHeliosu} {soubor} překlad neproběhl!!!")
-                    vysledkyPrekladuSouboru.append({vyslZprava})
-        zprava = ''            
-        for v in vysledkyPrekladuSouboru:
-            vstring = v[0]
-            zprava = zprava + '\n' + vstring
-        return zprava
+    #         print(f"Poslední změna {soubor} proběhla: {datumSouboru}")
+    
+            except:
+                datumSouboruDen = 'neni'
 
-    def ulozeniVysledku(vysledky):
-        with open('probehlpreklad.txt', 'w') as filehandle:
-            for listitem in vysledky:
-                filehandle.write('%s\n' % listitem)
+            if datumSouboruDen == datumDnesniDen:
+                vyslZprava = (f"{self.verzeHeliosu} {soubor} je OK")
+                vysledkyPrekladuSouboru.append([vyslZprava])
+                vysledkyOKKO.append(1)
+            elif datumSouboruDen == 'neni':
+                vyslZprava = (f"{self.verzeHeliosu} soubor {soubor} neni ve složce!!!")
+                vysledkyPrekladuSouboru.append([vyslZprava])
+                vysledkyOKKO.append(0)
+            else:
+                vyslZprava = (f"{self.verzeHeliosu} {soubor} překlad neproběhl!!!")
+                vysledkyPrekladuSouboru.append([vyslZprava])
+                vysledkyOKKO.append(0)
+            zprava = ''            
+            for v in vysledkyPrekladuSouboru:
+                vstring = v[0]
+                zprava = zprava + '\n' + vstring 
 
-    def posliEmail(config_soubor,
-                   textZpravy,
-                   predmet = 'Noční překlady',
+        return zprava, vysledkyOKKO
+
+
+    def vsechnyVerzeKontrola(self, configCesty):
+        self.configCesty = configCesty
+
+        with open(self.configCesty) as json_file:
+
+            configCesty = json.load(json_file) 
+        
+        preklady = Preklady()
+        
+        okko = []
+        konecnyVysledky = []
+        #print(configCesty)
+        for verzeHeliosu, souborCesta in configCesty.items():
+            if verzeHeliosu == 'HeO2-Beta':
+                heo2beta = preklady.kontrolaVerze(verzeHeliosu, souborCesta)[0]
+                konecnyVysledky.append(heo2beta)
+                seznamokkoheo2beta = preklady.kontrolaVerze(verzeHeliosu, souborCesta)[1]
+                okko.append(seznamokkoheo2beta)
+            elif verzeHeliosu == 'HeO2-RC':
+                heo2rc = preklady.kontrolaVerze(verzeHeliosu, souborCesta)[0]
+                konecnyVysledky.append(heo2rc)
+                seznamokkoheo2rc = preklady.kontrolaVerze(verzeHeliosu, souborCesta)[1]
+                okko.append(seznamokkoheo2rc)
+            elif verzeHeliosu == 'HeO3-Beta':
+                heo3beta = preklady.kontrolaVerze(verzeHeliosu, souborCesta)[0]
+                konecnyVysledky.append(heo3beta)
+                seznamokkoheo3beta = preklady.kontrolaVerze(verzeHeliosu, souborCesta)[1]
+                okko.append(seznamokkoheo3beta)
+            else:
+                heo3rc = preklady.kontrolaVerze(verzeHeliosu, souborCesta)[0]
+                konecnyVysledky.append(heo3rc)
+                seznamokkoheo3rc = preklady.kontrolaVerze(verzeHeliosu, souborCesta)[1]
+                okko.append(seznamokkoheo3rc)
+        
+        celkoveOKKO = []        
+        for i in range(len(okko)):
+            celkoveOKKO = celkoveOKKO + okko[i]
+
+        if sum(celkoveOKKO) == len(celkoveOKKO):
+            jevseok = True
+        else:
+            jevseok = False
+        
+        konecnazprava = ''
+        for v in konecnyVysledky:
+            vstring = v
+            konecnazprava = konecnazprava + '\n' + vstring    
+        return konecnazprava, jevseok
+          
+        
+
+    def posliEmail(self,
+                   config_soubor,
+                   textZpravy, 
+                   predmet,
                    attachment_location = ''):
         
+        msg = MIMEMultipart()
+
         with open(config_soubor) as json_file:
             
-            config = json.load(json_file)
-            
-            odesilatel = config["odesilatel"]
-            prijemci = config["prijemci"]
-            heslo = config["heslo"]
+            if predmet == True:
+                config = json.load(json_file)
+                odesilatel = config["odesilatel"]
+                prijemci = config["prijemciok"]
+                heslo = config["heslo"]
+                predmet = "Noční buildy OK"
+                msg['X-Priority'] = '3'
+            else:
+                config = json.load(json_file)
+                odesilatel = config["odesilatel"]
+                prijemci = config["prijemciko"]
+                heslo = config["heslo"]
+                predmet = "Noční buildy neproběhly!"
+                msg['X-Priority'] = '1'
 
-
-        msg = MIMEMultipart()
         msg['From'] = odesilatel
         msg['To'] = ", ".join(prijemci)
         msg['Subject'] = predmet
